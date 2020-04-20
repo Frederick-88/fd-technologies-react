43c75caf8eb1cdff4ab634175d1e42cf2e7f1159
Frederick-88 <frederickfd88@gmail.com>
2020-04-21 00:17:10 +0700

    COMMIT 2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React from "react";
+import Photo from "../assets/photo1.png";
 
-export default function componentName()  {
-        return (
-            <div>
-                <div className="row">
-                    <div className="col">
+export default function componentName() {
+  return (
+    <div className="container">
 
-                    </div>
+      <div className="row">
+        <div className="col mt-5">
+          <h1 className="pt-5">HI, I'm Frederick.</h1>
+          <h4>
+            I'm an upcoming FullStack Developer. Currently taking Fullstack
+            Major on Glints Academy Bootcamp Batam.
+          </h4>
 
-                    <div className="col">
-                        
-                    </div>
-                </div>
-            </div>
-        )
+          <h4 className=" mt-4">Take a look on My CV below --- </h4>
+          <button type="button" class="btn btn-outline-primary mt-4">Download My CV Here</button>
+        </div>
+
+        <div className="col">
+          <img src={Photo} className="img-fluid" alt="..." />
+        </div>
+      </div>
+      
+        <div>
+
+        </div>
+
+    </div>
+  );
 }
